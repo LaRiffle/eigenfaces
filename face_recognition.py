6c9137036117b41f964ccbe5ee35afbd49416499
Theo <theo.leffyr@gmail.com>
2018-02-25 13:33:07 +0000

    Complete preprocessor (B&W and precise crop of img) Add face recognition algorithm for this dataset /!\ Pb in classification

---
+++ face_recognition.py
@@ -0,0 +1,156 @@
+"""
+===================================================
+Faces recognition example using eigenfaces and SVMs
+===================================================
+
+
+"""
+from __future__ import print_function
+
+from os import listdir
+from os.path import isfile, join
+from time import time
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import fetch_lfw_people
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import RandomizedPCA
+from sklearn.svm import SVC
+
+
+
+
+
+###############################################################################
+# Load the data as numpy arrays
+
+#path = '/home/pi/Documents/test/'
+path = '/Users/ryffel/Documents/TPE/eigenfaces/'
+path = path + 'data/'
+
+files = [path+file for file in listdir(path) if isfile(join(path, file)) and file != '.DS_Store']
+
+n_samples = len(files)
+img = imageio.imread(files[0])
+h, w = img.shape
+print('SIZE of image :', h, 'x', w)
+
+# for machine learning we use the 2 data directly (as relative pixel
+# positions info is ignored by this model)
+X = []
+y = []
+target_names = ['max', 'victor']
+value = {'max': 0, 'victor': 1}
+for file in files:
+    print(file)
+    img = imageio.imread(file)
+    X.append(img.flatten())
+    filename = file.split('/')[-1]
+    label = filename.split('_')[0]
+    print(label)
+    y.append(value[label])
+
+X = np.array(X)
+y = np.array(y)
+n_features = X.shape[1]
+n_classes = len(target_names)
+
+print("Total dataset size:")
+print("n_samples: %d" % n_samples)
+print("n_features: %d" % n_features)
+print("n_classes: %d" % n_classes)
+
+
+###############################################################################
+# Split into a training set and a test set using a stratified k fold
+
+# split into a training and testing set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25)
+
+
+###############################################################################
+# Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
+# dataset): unsupervised feature extraction / dimensionality reduction
+n_components = 150
+
+print("Extracting the top %d eigenfaces from %d faces"
+      % (n_components, X_train.shape[0]))
+t0 = time()
+pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+print("done in %0.3fs" % (time() - t0))
+
+eigenfaces = pca.components_.reshape((n_components, h, w))
+
+print("Projecting the input data on the eigenfaces orthonormal basis")
+t0 = time()
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+print("done in %0.3fs" % (time() - t0))
+
+
+###############################################################################
+# Train a SVM classification model
+
+print("Fitting the classifier to the training set")
+t0 = time()
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
+clf = clf.fit(X_train_pca, y_train)
+print("done in %0.3fs" % (time() - t0))
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+
+###############################################################################
+# Quantitative evaluation of the model quality on the test set
+
+print("Predicting people's names on the test set")
+t0 = time()
+y_pred = clf.predict(X_test_pca)
+print("done in %0.3fs" % (time() - t0))
+
+print(classification_report(y_test, y_pred, target_names=target_names))
+print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+
+###############################################################################
+# Qualitative evaluation of the predictions using matplotlib
+
+def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+
+# plot the result of the prediction on a portion of the test set
+
+def title(y_pred, y_test, target_names, i):
+    pred_name = target_names[y_pred[i]]
+    true_name = target_names[y_test[i]]
+    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+
+prediction_titles = [title(y_pred, y_test, target_names, i)
+                     for i in range(y_pred.shape[0])]
+
+plot_gallery(X_test, prediction_titles, h, w)
+
+# plot the gallery of the most significative eigenfaces
+
+#eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+#plot_gallery(eigenfaces, eigenface_titles, h, w)
+
+plt.show()
--- preprocessor.py
+++ preprocessor.py
@@ -2,16 +2,48 @@ import imageio
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+from PIL import Image
 
-path = '/home/pi/Documents/test/'
-files = [path+file for file in listdir(path) if isfile(join(path, file))]
+
+#path = '/home/pi/Documents/test/'
+path = '/Users/ryffel/Documents/TPE/eigenfaces/'
+path_source = path + 'images/'
+path_target = path + 'data/'
+
+files = [path_source+file for file in listdir(path_source) if isfile(join(path_source, file)) and file != '.DS_Store']
+
+
+# convert is necessary img in B&W
+if False:
+    for file in files:
+        print(file)
+        image_file = Image.open(file)  # open colour image
+        image_file = image_file.convert('L')  # convert image to black and white
+        image_file.save(file)
+
 
 for file in files:
     print(file)
+
+    # crop image
     img = imageio.imread(file)
-    print(img.shape)
+    #print(img.shape)
+    height = img.shape[0]
     width = img.shape[1]
-    img = img[:,int(0.25*width):int(0.75*width),:]
-    # img = np.apply_along_axis(np.mean, 2, img)
-    imageio.imwrite(path + 'test.jpg', img)
-    
+    filename = file.split('/')[-1]
+    label = filename.split('_')[0]
+    if label == 'max':
+        img = img[int(0.10*height):int(0.724*height), int(0.38*width):int(0.71*width)]
+    elif label == 'victor':
+        img = img[int(0.12*height):int(0.88*height), int(0.30 * width):int(0.70 * width)]
+    imageio.imwrite(path_target + filename, img)
+
+    # reduce quality
+    basewidth = 37
+    img = Image.open(path_target + filename)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    img.save(path_target + filename)
+
+
